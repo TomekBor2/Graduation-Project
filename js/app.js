d927dca47683228ec396e6589f614ea14ce3aa6f
tomek <tomaszbor4@gmail.com>
2023-06-15 20:53:57 +0200

    filtering finished

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 5000,
   touch: false
 })
+
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
