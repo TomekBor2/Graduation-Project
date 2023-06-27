ca192a28908941aeaec851224b10b493c0f1bab5
tomek <tomaszbor4@gmail.com>
2023-06-27 18:01:13 +0200

    cart continues

--- shopping-cart/main.js
+++ shopping-cart/main.js
@@ -45,9 +45,9 @@ let generateShop = () => {
             <div class="price-quantity">
                 <h2>$ ${price}</h2>
                 <div class="buttons">
-                    <i class="bi bi-dash-lg"></i>
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
                     <div id=${id} class="quantity">0</div>
-                    <i class="bi bi-plus-lg"></i>
+                    <i id=${id} class="bi bi-plus-lg"></i>
                 </div>
             </div>
         </div>
@@ -57,20 +57,25 @@ let generateShop = () => {
 
 generateShop()
 
-// let plusIcon = Array.from(document.querySelectorAll('.bi-plus-lg'))
+let plusIcon = Array.from(document.querySelectorAll('.bi-plus-lg'))
+let quantity = Array.from(document.querySelectorAll('.quantity'))
 
-// function addOne() {
-//     counter++
-//     .innerText += counter
-// }
+function increment() {
+    counter++
+    quantity[id].innerText += counter
+}
 
-// plusIcon.forEach((i) => {
-//     i.addEventListener('click', (e) => {
-//         console.log('clicked')
-//         // addOne()
-//     })
-// })
+plusIcon.forEach((i) => {
+    i.addEventListener('click', (e) => {
+        console.log('increment')
+        increment()
+    })
+})
 
-let increment = () => {};
-let decrement = () => {};
-let update = () => {};
+// let increment = (id) => {
+//   console.log(id)
+// };
+let decrement = (id) => {
+  console.log(id)
+};
+// let update = () => {};
